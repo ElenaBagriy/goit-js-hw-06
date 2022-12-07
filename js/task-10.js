3bda622e488920e711d99ffa4208bf5f25f711ef
ElenaBagriy <elena.bagriy1989@gmail.com>
2022-12-08 00:52:02 +0200

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const divBoxes = document.querySelector('#boxes');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const amountOfBoxes = document.querySelector('#controls>input');
+    
+createButton.addEventListener('click', () => createBoxes(amountOfBoxes.value));
+
+destroyButton.addEventListener('click', () => destroyBoxes());
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const createBoxes = amount => {
+  const addedDivEl = [];
+
+  for (let i = 0; i <= amount-1; i += 1) {
+    const divToAdd = document.createElement("div");
+    divToAdd.style.backgroundColor = getRandomHexColor();
+    divToAdd.style.width = `${30 + 10 * (i-1)}px`;
+    divToAdd.style.height = `${30 + 10 * (i-1)}px`;
+    addedDivEl.push(divToAdd);
+  }
+
+  divBoxes.append(...addedDivEl);
+  
+}
+
+const destroyBoxes = () => {
+  divBoxes.innerHTML = ``;
+  amountOfBoxes.value = ``;
 }
